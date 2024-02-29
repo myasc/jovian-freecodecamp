876f5a36500914dfcd5322bc63e66f6765a546f8
myasc <74025941+myasc@users.noreply.github.com>
2024-02-29 12:42:12 +0000

    excercise in dict and api route to list excercises

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+excs = [
+    {
+        "title": "moutain climbers",
+        "targets": ["core"],
+        "description":
+        "get down in a push up position and bring feet next to your hands, one at a time",
+        "image_src": "TODO-add excercise image url"
+    },
+    {
+        "title": "deadlift",
+        "targets": ["core", "glutes", "hamstrings"],
+        "description":
+        "get down in a push up position and bring feet next to your hands, one at a time",
+        "image_src": "TODO-add excercise image url"
+    },
+    {
+        "title": "bench press",
+        "targets": ["chest", "triceps"],
+        "description":
+        "get down in a push up position and bring feet next to your hands, one at a time",
+        "image_src": "TODO-add excercise image url"
+    },
+]
 
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Woooorld!'
+  return render_template('home.html', exc=excs, client="Anirudh")
+
+@app.route('/excercises')
+def list_exc():
+  return jsonify(excs)
 
 
 if __name__ == '__main__':
